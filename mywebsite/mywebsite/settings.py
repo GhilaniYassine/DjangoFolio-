d86708805f24b1aa6fd1db6276dee90bec79aea1
GhilaniYassine <ghilaniyassine11@gmail.com>
2025-01-29 12:51:56 +0100

    Just set done of linking the templates with the settings.py file and created the home() func in views

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -1,3 +1,5 @@
+
+import os 
 """
 Django settings for mywebsite project.
 
@@ -55,11 +57,11 @@ ROOT_URLCONF = 'mywebsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['base/templates'],
+        'DIRS': [os.path.join(BASE_DIR , 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template.context_processors.debug',
+                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
